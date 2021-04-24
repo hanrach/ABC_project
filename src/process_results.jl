96cfeb7803acefab5e534e34303e1c21c4c9d944
Tae Yoon LEE <dlxodbs100@hotmail.com>
2021-04-23 17:31:59 -0700

    preprocessing results done

--- src/process_results.jl
+++ src/process_results.jl
@@ -1,74 +1,45 @@
 using JLD
 using DataFrames
 using Dates
-using LinearAlgebra
-using Distributions
-using Random
 using CSV
 using Printf
-using CPUTime
 
-df_pre = DataFrame(CSV.File("data/covid19_bc_pre_vaccine.csv"))
-df_post = DataFrame(CSV.File("data/covid19_bc_post_vaccine.csv"))
+function create_weekly_data(data_name,output_name)
+    df = DataFrame(CSV.File(data_name))
 
-# """ Pre-vaccine analysis """
-tot_days = size(df_pre)[1]
-time_interval = 7
+    # """ Pre-vaccine analysis """
+    tot_days = size(df)[1]
+    time_interval = 7
 
-y_pre = hcat([df_pre[:S],df_pre[:I],df_pre[:R]]...)[1:7:tot_days,:]
-y_pre = y_pre./1e4
-y_dim = size(y_pre)[1]
+    df_weekly = df[1:time_interval:tot_days,:]
 
-df_weekly = df_pre[1:7:tot_days,:]
-scale_SIR(SIR) = SIR./1e4
-transform!(df_weekly,[:S] => ByRow(scale_SIR) => :S)
-transform!(df_weekly,[:I] => ByRow(scale_SIR) => :I)
-transform!(df_weekly,[:R] => ByRow(scale_SIR) => :R)
+    scale_SIR(SIR) = SIR./1e4
 
-CSV.write("data/covid19_bc_pre_vaccine_weekly.csv",df_weekly)
+    transform!(df_weekly,[:S] => ByRow(scale_SIR) => :S)
+    transform!(df_weekly,[:I] => ByRow(scale_SIR) => :I)
+    transform!(df_weekly,[:R] => ByRow(scale_SIR) => :R)
 
-mcmc = load("results/abc_mcmc_solution_500.jld")["abc_mcmc_solutions"]
+    CSV.write(output_name,df_weekly)
+end
 
-long_mcmc = map(x-> mcmc[x,:,:],[1;2;3])
-using RCall
+function process_result(data_path,data_name)
+    dat = load(data_path)
+    key = [i for i in keys(dat)]
+    dat = dat[key[1]]
+    long = map(x-> dat[x,:,:],[1;2;3])
+    @rput long
+    R"saveRDS(long,$data_name)"
+end
 
-@rput long_mcmc
-R"saveRDS(long_mcmc,'mcmc.rds')"
+# save weekly data
+covid_csv_files = readdir("data")
+covid_csv_files = covid_csv_files[occursin.("covid19_bc_",covid_csv_files)]
+output_names = "data/weekly/" .* covid_csv_files
+covid_csv_files = "data/" .* covid_csv_files
+create_weekly_data.(covid_csv_files,output_names)
 
-smc = load("results/abc_smc_solution_500.jld")["abc_smc_solutions"]
-
-long_smc = map(x-> smc[x,:,:],[1;2;3])
-@rput long_smc
-R"saveRDS(long_smc,'smc.rds')"
-
-
-
-# """ Post-vaccine analysis """
-tot_days = size(df_post)[1]
-time_interval = 7
-
-y_pre = hcat([df_pre[:S],df_pre[:I],df_pre[:R]]...)[1:7:tot_days,:]
-y_pre = y_pre./1e4
-y_dim = size(y_pre)[1]
-
-df_weekly = df_pre[1:7:tot_days,:]
-scale_SIR(SIR) = SIR./1e4
-transform!(df_weekly,[:S] => ByRow(scale_SIR) => :S)
-transform!(df_weekly,[:I] => ByRow(scale_SIR) => :I)
-transform!(df_weekly,[:R] => ByRow(scale_SIR) => :R)
-
-CSV.write("data/covid19_bc_pre_vaccine_weekly.csv",df_weekly)
-
-mcmc = load("results/abc_mcmc_solution_500_post.jld")["abc_mcmc_solutions"]
-
-long_mcmc = map(x-> mcmc[x,:,:],[1;2;3])
-using RCall
-
-@rput long_mcmc
-R"saveRDS(long_mcmc,'mcmc.rds')"
-
-smc = load("results/abc_smc_solution_500_post.jld")["abc_smc_solutions"]
-
-long_smc = map(x-> smc[x,:,:],[1;2;3])
-@rput long_smc
-R"saveRDS(long_smc,'smc.rds')"
+# save results as rds files
+results = readdir("results/covid")
+output_names = "results/covid_rds/" .* replace.(results,".jld" => ".rds")
+results = "results/covid/" .* results
+process_result.(results,output_names)
